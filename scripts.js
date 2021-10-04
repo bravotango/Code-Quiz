1ca396db9f153aff65c7cec43b6db921d8ec72fd
seattleriot <seattleriot@hotmail.com>
2021-10-04 10:36:06 -0700

    code clean up: organization, refactor, added comments

--- scripts.js
+++ scripts.js
@@ -4,82 +4,79 @@ const questions = [
     id: 1,
     question: "What is not a JavaScript data type?",
     choices: [
-      { id: 1, answer: "string", value: "s", correct: false },
-      { id: 2, answer: "var", value: "v", correct: true },
-      { id: 3, answer: "object", value: "o", correct: false },
-      { id: 4, answer: "undefined", value: "u", correct: false },
+      { id: 1, answer: "string", value: false },
+      { id: 2, answer: "var", value: true },
+      { id: 3, answer: "object", value: false },
+      { id: 4, answer: "undefined", value: false },
     ],
   },
   {
     id: 2,
-    question:
-      "What key word is not a valid way to declare a JavaScript variable?",
+    question: "What key word is not a valid way to declare a variable?",
     choices: [
-      { id: 1, answer: "const", value: "c", correct: false },
-      { id: 2, answer: "var", value: "v", correct: false },
-      { id: 3, answer: "let", value: "l", correct: false },
-      { id: 4, answer: "func", value: "f", correct: true },
+      { id: 1, answer: "const", value: false },
+      { id: 2, answer: "var", value: false },
+      { id: 3, answer: "let", value: false },
+      { id: 4, answer: "func", value: true },
     ],
   },
   {
     id: 3,
     question: "Are Java and JavaScript related?",
     choices: [
-      { id: 1, answer: "Yes", value: "Y", correct: false },
-      { id: 2, answer: "No", value: "N", correct: true },
+      { id: 1, answer: "Yes", value: false },
+      { id: 2, answer: "No", value: true },
       {
         id: 3,
         answer: "Yes. Before 2000 they were cousins",
-        value: "x",
-        correct: false,
+        value: false,
       },
       {
         id: 4,
         answer: "No. After 2000 they separated",
-        value: "x",
-        correct: false,
+        value: false,
       },
     ],
   },
-
   {
     id: 4,
     question: "JavaScript document cookies are all of the following, except?",
     choices: [
-      { id: 1, answer: "Tasty edibles", value: "e", correct: true },
-      { id: 2, answer: "Cookies are data", value: "d", correct: false },
+      { id: 1, answer: "Tasty edibles", value: true },
+      { id: 2, answer: "Cookies are data", value: false },
       {
         id: 3,
         answer: "Cookies are stored in small text files",
-        value: "x",
-        correct: false,
+        value: false,
       },
       {
         id: 4,
         answer: "Cookies are stored on your computer",
-        value: "x",
-        correct: false,
+
+        value: false,
       },
     ],
   },
 ];
 
 // HTML elements to quickly hook into
-const startBtn = document.getElementById("start");
-const quizEl = document.getElementById("quiz");
-const quizNextQuestionBtn = document.getElementById("next");
-const quizCompleteEl = document.getElementById("quizComplete");
-const timerEl = document.getElementById("clock");
-const recordScoreBtn = document.getElementById("recordScore");
-const scoreBoard = document.getElementById("scoreBoard");
-const recordScoreForm = document.getElementById("recordScoreForm");
-const quizQuestionEl = document.querySelector("h2");
-const quizQuestionOlEl = document.querySelector("#quizContainer ol");
-const highScoresBtn = document.getElementById("highScoresBtn");
-const backToQuizBtn = document.getElementById("backToQuizBtn");
 const highScoresContainer = document.getElementById("highScoresContainer");
 const quizContainer = document.getElementById("quizContainer");
-const highScoresList = document.querySelector("#highScoresContainer ol");
+
+const quizEl = document.getElementById("quiz");
+const timerEl = document.getElementById("clock");
+const quizQuestionEl = document.querySelector("h2#question");
+const quizQuestionOlEl = document.querySelector("#quizContainer ol");
+const scoreBoardEl = document.getElementById("scoreBoard");
+const highScoresListOlEl = document.querySelector("#highScoresContainer ol");
+
+const recordScoreForm = document.getElementById("recordScoreForm");
+
+const startBtn = document.getElementById("start");
+const quizNextQuestionBtn = document.getElementById("next");
+const recordScoreBtn = document.getElementById("recordScore");
+const highScoresBtn = document.getElementById("highScoresBtn");
+const backToQuizBtn = document.getElementById("backToQuizBtn");
 const cancelRecordScoreBtn = document.getElementById("cancelRecordScoreBtn");
 
 // current state variables
@@ -107,46 +104,49 @@ function initializeQuiz() {
 
 // initialize Scoreboard
 function initializeScoreBoard() {
-  scoreBoard.innerHTML = "";
+  scoreBoardEl.innerHTML = "";
   timerEl.textContent = "";
   recordScoreBtn.style.display = "none";
   recordScoreForm.style.display = "none";
 }
 
 // Event listeners
-// record score button
-recordScoreBtn.addEventListener("click", displayRecordScoreForm);
+// is the start/restart quiz button clicked?
+startBtn.addEventListener("click", startBtnClickHandler);
 
-cancelRecordScoreBtn.addEventListener("click", closeRecordScoreForm);
+// next question button clicked?
+quizNextQuestionBtn.addEventListener("click", quizNextQuestionBtnClickHandler);
+
+// record score button
+recordScoreBtn.addEventListener("click", recordScoreBtnClickHandler);
 
 // is view high scores button clicked?
-highScoresBtn.addEventListener("click", showHighScores);
+highScoresBtn.addEventListener("click", highScoresBtnClickHandler);
 
 // is back to quiz button clicked?
-backToQuizBtn.addEventListener("click", backToQuiz);
+backToQuizBtn.addEventListener("click", backToQuizBtnClickHandler);
 
-// is a radio answer button clicked?
-quizEl.addEventListener("click", function (event) {
-  const element = event.target;
-  // check if click matches a input type radio button
-  if (element.matches("input[type='radio']")) {
-    latestRadioClick = element;
-    quizNextQuestionBtn.disabled = false;
-  }
-});
+// cancel record score button
+cancelRecordScoreBtn.addEventListener(
+  "click",
+  cancelRecordScoreBtnClickHandler
+);
 
-// is the start/restart quiz button clicked?
-startBtn.addEventListener("click", function () {
+// is a radio button clicked?
+quizEl.addEventListener("click", quizElClickHandler);
+
+// start quiz button clicked - initialize quiz & scoreboard, show quiz, start timer
+function startBtnClickHandler() {
   initializeQuiz();
   initializeScoreBoard();
   startBtn.style.display = "none";
   quizEl.style.display = "block";
   presentQuestion();
   stopWatch();
-});
+}
 
-// next question button clicked?
-quizNextQuestionBtn.addEventListener("click", function () {
+// next question button clicked
+function quizNextQuestionBtnClickHandler() {
   if (checkAnswer(latestRadioClick) === true) {
     correctTotal++;
   } else {
@@ -161,7 +161,61 @@ quizNextQuestionBtn.addEventListener("click", function () {
   currentQuestion++;
   this.disabled = true;
   presentQuestion();
-});
+}
+
+// display the record score form
+function recordScoreBtnClickHandler() {
+  recordScoreForm.style.display = "block";
+  recordScoreBtn.style.display = "none";
+  startBtn.style.display = "none";
+}
+
+// view high scores button clicked - show high scores, hide quiz
+function highScoresBtnClickHandler() {
+  const highScores = getHighScores();
+  backToQuizBtn.style.display = "block";
+  quizContainer.style.display = "none";
+  highScoresBtn.style.display = "none";
+  highScoresContainer.style.display = "block";
+  if (!highScores) {
+    highScoresListOlEl.innerHTML = "<li>No high scores have been recorded</li>";
+    return;
+  }
+
+  const numberOfScoresToDisplay =
+    highScores.length < 10 ? highScores.length : 10;
+  highScoresListOlEl.innerHTML = "<li><p>Rank</p><p>Score</p><p>Name</p></li>";
+  for (let i = 0; i < numberOfScoresToDisplay; i++) {
+    let gameHTML = getScoreHTML(highScores[i], i + 1);
+    console.log(gameHTML);
+    highScoresListOlEl.appendChild(gameHTML);
+  }
+}
+
+// back to quiz button clicked - hide high scores, show quiz
+function backToQuizBtnClickHandler() {
+  quizContainer.style.display = "block";
+  highScoresContainer.style.display = "none";
+  highScoresBtn.style.display = "block";
+  backToQuizBtn.style.display = "none";
+}
+
+// hide the record score form
+function cancelRecordScoreBtnClickHandler() {
+  recordScoreForm.style.display = "none";
+  recordScoreBtn.style.display = "block";
+  startBtn.style.display = "block";
+}
+
+// check if radio button was clicked, enable next question button
+function quizElClickHandler(event) {
+  const element = event.target;
+  // check if click matches a input type radio button
+  if (element.matches("input[type='radio']")) {
+    latestRadioClick = element;
+    quizNextQuestionBtn.disabled = false;
+  }
+}
 
 // timer
 function stopWatch() {
@@ -178,6 +232,92 @@ function stopWatch() {
   }, 100);
 }
 
+// simple writeTimer function since called in multiple places
+function writeTimer() {
+  timerEl.textContent = `Time left: ${timer.toFixed(1)}`;
+}
+
+function presentQuestion() {
+  if (isMoreQuestions() === false) {
+    // no more questions or time's up, isQuestionsDone() says we are done - let's jump out
+    return;
+  }
+  // we have a question - let's ask it
+  quizQuestionEl.innerHTML = questions[currentQuestion].question;
+  // remove any previous choices listed
+  quizQuestionOlEl.innerHTML = "";
+  // list the current question choices
+  listChoices();
+}
+
+// based on currentQuestion, list the question choices,
+function listChoices() {
+  for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
+    // create the list element
+    let choice = document.createElement("li");
+
+    // create the radio form elements
+    let radio = document.createElement("input");
+    let label = document.createElement("label");
+
+    // populate radio element
+    radio.dataset.id = questions[currentQuestion].choices[i].id;
+    radio.dataset.questionId = currentQuestion;
+    radio.type = "radio";
+    radio.value = questions[currentQuestion].choices[i].value;
+    radio.name = "question" + (currentQuestion + 1);
+    radio.id = "answer" + (i + 1);
+
+    // populate label element
+    label.htmlFor = "answer" + (i + 1);
+    label.innerHTML = questions[currentQuestion].choices[i].answer;
+
+    quizQuestionOlEl.appendChild(choice);
+    choice.appendChild(radio);
+    choice.appendChild(label);
+  }
+}
+
+// check if the answer is correct - return true or false
+function checkAnswer(el) {
+  const questionId = el.dataset.questionId;
+  const correctAnswer = questions[questionId].choices.find((choice) => {
+    if (choice.value === true) {
+      return true;
+    }
+  });
+  return el.dataset.id == correctAnswer.id ? true : false;
+}
+
+// returns boolean if no more question, stops timer & calls gameOver if false
+function isMoreQuestions() {
+  // are we done with questions? If yes, let hop in
+  if (currentQuestion >= questions.length) {
+    // stop the timer
+    clearInterval(timeInterval);
+    if (timer < 0) {
+      timer = 0;
+    }
+    // update the timer for user
+    writeTimer();
+    quizEl.style.display = "none";
+
+    // it's over, call gameOver
+    gameOver();
+    return false;
+  }
+  // we are not done with questions
+  return true;
+}
+
+function getScoreHTML(game, rank) {
+  let gameHTML = document.createElement("li");
+  gameHTML.innerHTML = `<p>${rank}</p><p>${game.score.toFixed(1)}</p><p>${
+    game.name
+  }</p>`;
+  return gameHTML;
+}
+
 // game over - hide quiz - display quiz stats
 function gameOver() {
   startBtn.innerText = "Restart Quiz?";
@@ -186,25 +326,24 @@ function gameOver() {
     timer = 0;
     const timedOut = document.createElement("p");
     timedOut.textContent = "Time expired";
-    quizCompleteEl.appendChild(timedOut);
   }
 
   // display total answered
   const pAnswered = document.createElement("p");
   pAnswered.textContent = `Answered: ${currentQuestion} / ${questions.length}`;
-  scoreBoard.appendChild(pAnswered);
+  scoreBoardEl.appendChild(pAnswered);
 
   // display total correct
   const pCorrect = document.createElement("p");
   pCorrect.textContent = `Correct: ${correctTotal}`;
-  scoreBoard.appendChild(pCorrect);
+  scoreBoardEl.appendChild(pCorrect);
 
   // display total time penalties
   const pTimePenalties = document.createElement("p");
   pTimePenalties.textContent = `Penalties: ${timePenalties}, subtracted ${
     timePenalties * penaltyTime
   } seconds`;
-  scoreBoard.appendChild(pTimePenalties);
+  scoreBoardEl.appendChild(pTimePenalties);
 
   // display calculated score
   const pScore = document.createElement("p");
@@ -212,27 +351,13 @@ function gameOver() {
   pScore.innerHTML = `<strong>${score.toFixed(1)}</strong>`;
   pScore.setAttribute("title", `${score}`);
   pScore.setAttribute("id", "quizScore");
-  scoreBoard.appendChild(pScore);
+  scoreBoardEl.appendChild(pScore);
 
   const pScoreDefinition = document.createElement("p");
   pScoreDefinition.setAttribute("class", "small");
-  pScoreDefinition.innerHTML = `Score calculated by: correct + (time left * correct) - penalties`;
-  scoreBoard.appendChild(pScoreDefinition);
+  pScoreDefinition.innerHTML = `Score calculated by: correct + (time left * correct) - wrong`;
+  scoreBoardEl.appendChild(pScoreDefinition);
 
-  recordScoreBtn.style.display = "block";
-  startBtn.style.display = "block";
-}
-
-// display the record score form
-function displayRecordScoreForm() {
-  recordScoreForm.style.display = "block";
-  recordScoreBtn.style.display = "none";
-  startBtn.style.display = "none";
-}
-
-// hide the record score form
-function closeRecordScoreForm() {
-  recordScoreForm.style.display = "none";
   recordScoreBtn.style.display = "block";
   startBtn.style.display = "block";
 }
@@ -246,7 +371,7 @@ function recordScore(event) {
   const scoreObj = {
     score: score,
     correct: correctTotal,
-    name: initials,
+    name: initials.toUpperCase(),
     date: Date.now(),
     penalties: timePenalties,
     timeLeft: timer,
@@ -275,83 +400,6 @@ function recordScore(event) {
   startBtn.style.display = "block";
 }
 
-function isMoreQuestions() {
-  // are we done with questions? If yes, let hop in
-  if (currentQuestion >= questions.length) {
-    // stop the timer
-    clearInterval(timeInterval);
-    if (timer < 0) {
-      timer = 0;
-    }
-    // update the timer for user
-    writeTimer();
-    quizEl.style.display = "none";
-
-    // it's over, call gameOver
-    gameOver();
-    return true;
-  }
-  // we are not done with questions
-  return false;
-}
-
-function presentQuestion() {
-  if (isMoreQuestions() === true) {
-    // no more questions or time's up, isQuestionsDone() says we are done - let's jump out
-    return;
-  }
-  // we have a question - let's ask it
-  quizQuestionEl.innerHTML = questions[currentQuestion].question;
-  // remove any previous choices listed
-  quizQuestionOlEl.innerHTML = "";
-  // list the current question choices
-  listChoices();
-}
-
-// based on currentQuestion, list the question choices,
-function listChoices() {
-  for (let i = 0; i < questions[currentQuestion].choices.length; i++) {
-    // create the list element
-    let choice = document.createElement("li");
-
-    // create the radio form elements
-    let radio = document.createElement("input");
-    let label = document.createElement("label");
-
-    // populate radio element
-    radio.dataset.id = questions[currentQuestion].choices[i].id;
-    radio.dataset.questionId = currentQuestion;
-    radio.type = "radio";
-    //radio.value = questions[currentQuestion].choices[i].value;
-    radio.name = "question" + (currentQuestion + 1);
-    radio.id = "answer" + (i + 1);
-
-    // populate label element
-    label.htmlFor = "answer" + (i + 1);
-    label.innerHTML = questions[currentQuestion].choices[i].answer;
-
-    quizQuestionOlEl.appendChild(choice);
-    choice.appendChild(radio);
-    choice.appendChild(label);
-  }
-}
-
-// check if the answer is correct - return true or false
-function checkAnswer(el) {
-  const questionId = el.dataset.questionId;
-  const correctAnswer = questions[questionId].choices.find((choice) => {
-    if (choice.correct === true) {
-      return true;
-    }
-  });
-  return el.dataset.id == correctAnswer.id ? true : false;
-}
-
-// simple writeTimer function since called in multiple places
-function writeTimer() {
-  timerEl.textContent = `Time left: ${timer.toFixed(1)}`;
-}
-
 function getHighScores() {
   let highScores = JSON.parse(localStorage.getItem("highScores"));
   // sort highScores by score
@@ -362,40 +410,4 @@ function getHighScores() {
     });
   }
   return highScores;
-}
-
-function showHighScores() {
-  const highScores = getHighScores();
-  backToQuizBtn.style.display = "block";
-  quizContainer.style.display = "none";
-  highScoresBtn.style.display = "none";
-  highScoresContainer.style.display = "block";
-  if (!highScores) {
-    highScoresList.innerHTML = "<li>No high scores have been recorded</li>";
-    return;
-  }
-
-  const numberOfScoresToDisplay =
-    highScores.length < 10 ? highScores.length : 10;
-  highScoresList.innerHTML = "<li><p>Rank</p><p>Score</p><p>Name</p></li>";
-  for (let i = 0; i < numberOfScoresToDisplay; i++) {
-    let gameHTML = getScoreHTML(highScores[i], i + 1);
-    console.log(gameHTML);
-    highScoresList.appendChild(gameHTML);
-  }
-}
-
-function getScoreHTML(game, rank) {
-  let gameHTML = document.createElement("li");
-  gameHTML.innerHTML = `<p>${rank}</p><p>${game.score.toFixed(1)}</p><p>${
-    game.name
-  }</p>`;
-  return gameHTML;
-}
-
-function backToQuiz() {
-  quizContainer.style.display = "block";
-  highScoresContainer.style.display = "none";
-  highScoresBtn.style.display = "block";
-  backToQuizBtn.style.display = "none";
 }
